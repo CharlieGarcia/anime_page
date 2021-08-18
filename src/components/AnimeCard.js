6e415b839b9abf6c9c9cc8c15c411e1e80dd5e43
CharlieEscoboza <ing.charlie.garcia@gmail.com>
2021-08-17 22:20:57 -0400

    Add styled-components package

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -1,21 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  display: flex;
+`;
+
+const CardDetails = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  min-width: calc(100% - (284px + 20px));
+`;
+
+const ImageWrapper = styled.img.attrs((props) => ({
+  src: props.imageUrl
+}))`
+  margin: 0 20px 0 0;
+`;
+
+const StyledLink = styled(Link).attrs((props) => ({
+  to: props.to
+}))`
+  border-radius: 20px;
+  box-shadow: 10px 10px 30px rgba(0, 0, 0, .4);
+  color: #000;
+  display: block;
+  margin: 20px 0;
+  text-decoration: none;
+  overflow: hidden;
+`;
 
 function AnimeCard({ id, attributes }) {
   const { subtype = '', titles = {}, posterImage = {}, episodeCount = '', episodeLength = '' } = attributes;
   const { en: englishTitle = '', en_jp: enGlishJapaneseTitle = '', ja_jp: titleRomaji = '' } = titles;
-  const { medium: posterMediumUrl } = posterImage;
+  const { small: posterImageUrl } = posterImage;
   const titleEnglish = englishTitle || enGlishJapaneseTitle;
 
+
   return (
-    <Link to={`/details/${id}`}>
-      <img src={posterMediumUrl} alt={titleEnglish} />
-      <p>Japanese Title: {titleRomaji}</p>
-      <p>English Title: {titleEnglish}</p>
-      <p>Episodes: {episodeCount}</p>
-      <p>SubType: {subtype}</p>
-      <p>Episode Length in minutes: {episodeLength || "N/A"}</p>
-    </Link>
+    <StyledLink to={`/details/${id}`}>
+      <Card>
+        <ImageWrapper imageUrl={posterImageUrl} />
+        <CardDetails>
+          <p>Japanese Title: {titleRomaji}</p>
+          <p>English Title: {titleEnglish}</p>
+          <p>Episodes: {episodeCount || "N/A"}</p>
+          <p>SubType: {subtype}</p>
+          <p>Episode Length in minutes: {episodeLength || "N/A"}</p>
+        </CardDetails>
+      </Card>
+    </StyledLink>
   );
 }
 
