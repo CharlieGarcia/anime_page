374dcbb142b270d866f0b3c398036a2987c06a52
Charlie Escoboza Garcia <ing.charlie.garcia@gmail.com>
2021-10-27 08:27:19 -0400

    Add prettier config (#9)

--- src/views/Browse.js
+++ src/views/Browse.js
@@ -15,7 +15,6 @@ import {
   ITEMS_PER_PAGE
 } from '../constants';
 
-
 class Browse extends React.Component {
   constructor(props) {
     super(props);
@@ -42,66 +41,80 @@ class Browse extends React.Component {
       elementValue = evt.target ? evt.target.value : null;
 
     if (elementName && elementValue !== null) {
-      const newState = Object.assign({}, this.state.searchFields, { [elementName]: elementValue });
+      const newState = Object.assign({}, this.state.searchFields, {
+        [elementName]: elementValue
+      });
 
       this.setState(() => ({
         searchFields: newState
       }));
     }
-  }
+  };
 
   updateSelectField = (evt) => {
     const element = evt.target || null,
       elementName = element ? element.getAttribute('name') : null,
-      selectedOption = element ? element.options[element.selectedIndex].value : '',
-      newState = Object.assign({}, this.state.searchFields, { [elementName]: selectedOption });
+      selectedOption = element
+        ? element.options[element.selectedIndex].value
+        : '',
+      newState = Object.assign({}, this.state.searchFields, {
+        [elementName]: selectedOption
+      });
 
     this.setState(() => ({
       searchFields: newState
-    }))
-  }
+    }));
+  };
 
   updateAnimeList = (currentPage) => {
     const offset = currentPage > 1 ? currentPage * ITEMS_PER_PAGE : currentPage;
-    const searchParams = _reduce(this.state.searchFields, (result, value, key) => {
-      if (value) {
-        if (key !== 'sort') {
-          result[`filter[${key}]`] = value;
-        } else {
-          result[`sort`] = value;
-        }
-      }
+    const searchParams =
+      _reduce(
+        this.state.searchFields,
+        (result, value, key) => {
+          if (value) {
+            if (key !== 'sort') {
+              result[`filter[${key}]`] = value;
+            } else {
+              result[`sort`] = value;
+            }
+          }
 
-      return result;
-    }, {}) || {};
+          return result;
+        },
+        {}
+      ) || {};
     const options = {
-      headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
-      'page[offset]': offset, 'page[limit]': ITEMS_PER_PAGE, ...searchParams
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      'page[offset]': offset,
+      'page[limit]': ITEMS_PER_PAGE,
+      ...searchParams
     };
 
-
-    return fetch('/anime', options)
-      .then((resp) => {
-        this.setState(() => ({
-          animeList: _get(resp, 'data.data', []),
-          count: _get(resp, 'data.meta.count', 0),
-          currentPage,
-          status: 'done'
-        }))
-      });
-  }
+    return fetch('/anime', options).then((resp) => {
+      this.setState(() => ({
+        animeList: _get(resp, 'data.data', []),
+        count: _get(resp, 'data.meta.count', 0),
+        currentPage,
+        status: 'done'
+      }));
+    });
+  };
 
   fetchAnimes = (evt) => {
     this.setState({ status: 'searching' });
     evt.preventDefault();
     this.updateAnimeList(1);
-  }
+  };
 
   updateCurrentPage = (evt) => {
     this.updateAnimeList(parseInt(evt.target.outerText)).then(() => {
       window.scrollTo(0, 0);
     });
-  }
+  };
 
   clearFilters = () => {
     this.setState({
@@ -110,7 +123,7 @@ class Browse extends React.Component {
       currentPage: 1,
       status: ''
     });
-  }
+  };
 
   render() {
     return (
@@ -121,16 +134,20 @@ class Browse extends React.Component {
           updateTextField={this.updateTextField}
           updateSelectField={this.updateSelectField}
           fetchAnimes={this.fetchAnimes}
-          clearFilters={this.clearFilters} />
+          clearFilters={this.clearFilters}
+        />
         {this.state.status === 'searching' ? 'Loading...' : null}
-        {this.state.status === 'done' ? <AnimeList list={this.state.animeList} /> : null}
-        {this.state.count
-          ? <Pagination
+        {this.state.status === 'done' ? (
+          <AnimeList list={this.state.animeList} />
+        ) : null}
+        {this.state.count ? (
+          <Pagination
             total={this.state.count}
             itemsPerPage={ITEMS_PER_PAGE}
             currentPage={this.state.currentPage}
-            updateCurrentPage={this.updateCurrentPage} />
-          : null}
+            updateCurrentPage={this.updateCurrentPage}
+          />
+        ) : null}
       </div>
     );
   }
