6715e615b42f72a2993134e15808696c3394f2a4
CharlieEscoboza <ing.charlie.garcia@gmail.com>
2021-11-08 03:08:48 -0400

    Add detail page

---
+++ src/views/Detail.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import _get from 'lodash/get';
+import { fetch } from "../helpers/request";
+
+function Detail() {
+  const { id } = useParams();
+  const [info, setInfo] = useState({});
+
+  useEffect(async () => {
+    const _info = await fetch(`/anime/${id}`);
+
+    setInfo(_get(_info, 'data.data') || {});
+  });
+
+  return (
+    <div>Detail page
+      <p>
+        `${JSON.stringify(info, null, 2)}`
+      </p>
+    </div>
+  );
+}
+
+export default Detail;
