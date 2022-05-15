8a2a4ebf0bbb6b82ec546c308966b1ea5b0db667
Charlie Escoboza Garcia <ing.charlie.garcia@gmail.com>
2022-05-15 11:25:51 -0400

    Feature/add home trending animes (#15)
    * Merge master
    
    * fix: precommit execution
    
    * Update stale branch
    
    * Remove duplicated line pre-commit hook

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import _get from 'lodash/get';
+import AnimeList from '../components/AnimeList';
+import { fetch } from '../helpers/request';
 
-function Home({ props }) {
+function Home() {
+  const [animeList, setAnimeList] = useState([]);
+
+  useEffect(async () => {
+    const animes = await fetch('/trending/anime');
+
+    setAnimeList(_get(animes, 'data.data', []));
+  }, []);
+
   return (
     <div>
       <h1>My Anime Page</h1>
       <p>
-        This is a page for testing create-react-app and make queries to <a href="https://kitsu.docs.apiary.io/#introduction/json:api" target="_blank" rel="noopener noreferrer">kitsu API</a>.
+        This is a page for testing create-react-app and make queries to{' '}
+        <a
+          href="https://kitsu.docs.apiary.io/#introduction/json:api"
+          target="_blank"
+          rel="noopener noreferrer">
+          kitsu API
+        </a>
+        .
         <br />
-        For browsering animes from the API list, please visit our <NavLink to="/browse">Browse section</NavLink> ;)
+        For browsering animes from the API list, please visit our{' '}
+        <NavLink to="/browse">Browse section</NavLink> ;)
       </p>
+      <h2>Top 10 Trending Animes</h2>
+      <AnimeList list={animeList} />
     </div>
   );
 }
