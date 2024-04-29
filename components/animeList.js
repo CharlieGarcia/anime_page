19314117fcfd5e8a9af44cccd3f55487a04d9cf4
Charlie Escoboza Garcia <ing.charlie.garcia@gmail.com>
2024-04-29 12:40:45 -0400

    Enh: Change components file name (#52)

---
+++ components/animeList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import AnimeCard from './animeCard';
+import { Box } from '@mui/material';
+
+const styles = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(4, auto)',
+  gridTemplateRows: 'auto',
+  gap: '30px'
+};
+
+const AnimeList = ({ list = [] }) => {
+  return (
+    <Box sx={styles}>
+      {!list.length
+        ? 'Content not found. Please try with other parameters'
+        : null}
+      {list.length
+        ? list.map((anime, index) => <AnimeCard key={index} {...anime} />)
+        : null}
+    </Box>
+  );
+};
+
+export default AnimeList;
