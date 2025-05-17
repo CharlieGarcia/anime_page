80d987e554d61f48f791b55ad6f662171a6033bc
Charlie Garcia <ing.charlie.garcia@gmail.com>
2025-05-17 12:44:24 -0400

    Update related page (#63)

--- pages/related/[id].js
+++ pages/related/[id].js
@@ -1,58 +1,57 @@
 import React, { useEffect, useState } from 'react';
-import { Box } from '@mui/material';
-import { useRouter } from 'next/router';
+import { Box, Typography } from '@mui/material';
 import _get from 'lodash/get';
-import { Layout } from '../../components/layout';
-import { fetch } from '../../helpers/request';
-import AnimeList from '../../components/animeList';
+import { useRouter } from 'next/router';
+import { Layout } from '@/components/layout';
+import { fetch } from '@/helpers/request';
+import AnimeList from '@/components/animeList';
 
 function Related() {
   const router = useRouter();
   const { id } = router.query;
-  const [state, setState] = useState({
-    relatedAnimes: [],
-    infoStatus: false
-  });
+  const [relatedAnimes, setRelatedAnimes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    setState((existingState) => ({
-      ...existingState,
-      infoStatus: true
-    }));
+    if (router.isReady) {
+      // Had to do it this way because of the response size when calling from the getServerSideProps
+      const fetchRelatedAnimes = async () => {
+        const relatedAnimesResponse = await fetch(
+          `/categories/${id}/relationships/anime`
+        );
+        const relatedAnimesIds = _get(
+          relatedAnimesResponse,
+          'data.data' || []
+        ).map((anime) => anime.id);
+        const relatedAnimes = await Promise.allSettled(
+          relatedAnimesIds.map((animeId) => fetch(`/anime/${animeId}`))
+        );
 
-    const fetchRelatedAnimesIds = async () => {
-      if (!router.isReady) return;
-      const relatedAnimesResponse = await fetch(
-        `/categories/${id}/relationships/anime`
-      );
-      const relatedAnimesIds = _get(
-        relatedAnimesResponse,
-        'data.data' || []
-      ).map((anime) => anime.id);
-      const relatedAnimes = await Promise.allSettled(
-        relatedAnimesIds.map((animeId) => fetch(`/anime/${animeId}`))
-      );
+        return relatedAnimes
+          .filter(({ status }) => status === 'fulfilled')
+          .map(({ value }) => _get(value, 'data.data', {}));
+      };
 
-      setState((existingState) => ({
-        ...existingState,
-        relatedAnimes:
-          relatedAnimes
-            .filter(({ status }) => status === 'fulfilled')
-            .map(({ value }) => value.data.data) || [],
-        infoStatus: false
-      }));
-    };
-
-    fetchRelatedAnimesIds();
+      fetchRelatedAnimes()
+        .then((result) => {
+          setIsLoading(false);
+          setRelatedAnimes(result);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setRelatedAnimes([]);
+          setIsLoading(false);
+        });
+    }
   }, [router.isReady, id]);
 
   return (
     <Layout>
-      {state.infoStatus && 'Loading ...'}
       <Box>
-        {state.relatedAnimes.length ? (
-          <AnimeList list={state.relatedAnimes} />
-        ) : null}
+        {isLoading ? <Typography>Loading...</Typography> : null}
+        {error ? <Typography>{error}</Typography> : null}
+        {relatedAnimes?.length ? <AnimeList list={relatedAnimes} /> : null}
       </Box>
     </Layout>
   );
