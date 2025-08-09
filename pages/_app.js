417500c972a4fe82bd57358d8b9fe6e891622465
CharlieGarcia <ing.charlie.garcia@gmail.com>
2025-08-09 04:38:21 -0400

    Enable dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,35 @@
 import React from 'react';
-import '../styles/reset.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material';
+import ColorModeContext from '../context/theme';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [mode, setMode] = React.useState('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      }
+    }),
+    []
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode
+        }
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
