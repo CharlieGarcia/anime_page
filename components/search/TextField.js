442c58ca9fbf4cee24c3cacb2756ca9938611656
Charlie Garcia <ing.charlie.garcia@gmail.com>
2025-08-09 06:38:17 -0400

    Update @mui/icons-material and @mui/material (#79)

--- components/search/TextField.js
+++ components/search/TextField.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { FormControl, TextField } from '@mui/material';
 
-const CustomTextField = ({ name, label, id, value, onChange }) => (
-  <FormControl>
+const CustomTextField = ({ name, label, id, value, onChange, fullWidth = false }) => (
+  <FormControl fullWidth={fullWidth}>
     <TextField
       label={label || name}
       variant="outlined"
