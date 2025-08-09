495912279feb397df865f2ca9d1df127f223410d
Charlie Garcia <ing.charlie.garcia@gmail.com>
2025-08-09 04:42:54 -0400

    Revert "Enable dark mode (#69)" (#70)
    This reverts commit 63fc43089b22f6e7ab3c6b3a111370ceab15832b.

--- components/header.js
+++ components/header.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
 import { Box } from '@mui/material';
-import { ThemeToggle } from './themeToggle';
 
 export const Header = () => {
-  const headerStyles = [(theme) => ({
-    backgroundColor: theme.palette.background.default,
+  const headerStyles = {
+    backgroundColor: 'white',
     padding: '24px',
     position: 'sticky',
     top: '0',
-    zIndex: '2',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  })];
+    zIndex: '2'
+  };
 
   return (
     <Box component="header" sx={headerStyles}>
       <Link href="/" style={{ textDecoration: 'none', fontSize: '24px' }}>
         My Anime
       </Link>
-      <ThemeToggle />
     </Box>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,6 @@
 import React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme, ThemeProvider } from '@mui/material';
-import ColorModeContext from '../context/theme';
+import '../styles/reset.css';
 
 export default function App({ Component, pageProps }) {
-  const [mode, setMode] = React.useState('light');
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      }
-    }),
-    []
-  );
-
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode
-        }
-      }),
-    [mode]
-  );
-
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-  );
+  return <Component {...pageProps} />;
 }
